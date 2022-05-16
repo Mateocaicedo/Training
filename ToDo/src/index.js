fced6210beff35678977be2d11720a53cd88bd42
MateoCaicedoW <mcaicedo@wawand.co>
2022-05-16 17:54:17 -0500

    ToDo with node

---
+++ ToDo/src/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express();
+const path = require('path')
+
+//Settings
+app.set('port',3000)
+app.set('view engine', 'ejs')
+app.set('views',path.join(__dirname, 'view'))
+app.engine('html', require('ejs').renderFile)
+
+//Routes
+app.use(require('./routes/routes'))
+
+//static files
+app.use(express.static(path.join(__dirname, 'public')))
+
+//Listening Server
+app.listen(app.get('port'), ()=>{
+    console.log("server on port ", app.get('port'))
+})
