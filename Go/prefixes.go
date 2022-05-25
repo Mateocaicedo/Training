5e9a39e75c34dda092c16d35ec8589170b3e5b8a
MateoCaicedoW <mcaicedo@wawand.co>
2022-05-25 11:59:06 -0500

    prefixes

--- Go/prefixes.go
+++ Go/prefixes.go
@@ -8,115 +8,110 @@ import (
 
 func main() {
 	n := 0.0
-	var cont_0 int
-	var cont_d int
-	fmt.Print("Ingrese una cantidad: ")
+	var count0 int
+	var countd int
+	fmt.Print("Enter a number: ")
 	fmt.Scanln(&n)
-	resultado := ""
+	generalResult := ""
 	if n < 1 {
-		resultado = "Ingrese un numero mayor que 0"
+		generalResult = "Enter a number greater than 0: "
 	} else if len(strconv.Itoa(int(n))) == 1 {
-		resultado = strconv.Itoa(int(n))
+		generalResult = strconv.Itoa(int(n))
 
 	} else {
 		n1 := strings.Split(strconv.Itoa(int(n)), "")
 
-		for i := 0; i < len(n1); i++ {
-			nuevo, _ := strconv.Atoi(n1[i])
-			if nuevo != 0 {
-				cont_d++
+		for i := len(n1) - 1; i >= 0; i-- {
+			new, _ := strconv.Atoi(n1[i])
+
+			if new != 0 {
+				countd++
 			}
 			if i != 0 {
-				cont_0++
+				count0++
+
 			}
+
 		}
 
-		if cont_0 == 1 {
-			for i := 0; i < cont_d; i++ {
-				resultado += n1[i]
-			}
+		if count0 == 1 {
+			generalResult = result2(countd, n1, "da")
+		} else if count0 == 2 {
+			generalResult = result2(countd, n1, "h")
 
-			if cont_d > 1 {
-				resultado2 := strings.Split(resultado, "")
+		} else if count0 <= 3 || count0 < 6 {
+			generalResult = result(countd, n1, "k", count0, 3)
 
-				resultado = resultado2[0] + "." + resultado2[1] + "da"
-			} else {
+		} else if count0 <= 6 || count0 < 9 {
+			generalResult = result(countd, n1, "M", count0, 6)
+		} else if count0 <= 9 || count0 < 12 {
 
-				resultado = resultado + "da"
-			}
+			generalResult = result(countd, n1, "G", count0, 9)
+		} else if count0 <= 12 || count0 < 15 {
 
-		} else if cont_0 == 2 {
-			for i := 0; i < cont_d; i++ {
-				resultado += n1[i]
-			}
-			if cont_d > 1 {
-				resultado2 := strings.Split(resultado, "")
+			generalResult = result(countd, n1, "T", count0, 12)
+		} else if count0 <= 15 || count0 < 18 {
 
-				resultado = resultado2[0] + "." + resultado2[1] + "h"
-			} else {
+			generalResult = result(countd, n1, "P", count0, 15)
+		} else if count0 <= 18 || count0 < 21 {
 
-				resultado = resultado + "h"
-			}
+			generalResult = result(countd, n1, "E", count0, 18)
+		} else if count0 <= 21 || count0 < 24 {
 
-		} else if cont_0 == 3 {
-			for i := 0; i < cont_d; i++ {
-				resultado += n1[i]
-			}
-			if cont_d > 1 {
-				resultado2 := strings.Split(resultado, "")
-				for i := 0; i < len(resultado2); i++ {
-					if i == 0 {
-						resultado = resultado2[0] + "."
-					} else if i != 0 {
-						resultado += resultado2[i]
-					}
-				}
-				resultado = resultado + "k"
+			generalResult = result(countd, n1, "Y", count0, 21)
+		} else if count0 <= 24 {
 
-			} else {
-
-				resultado = resultado + "k"
-			}
-
-		} else if cont_0 == 6 {
-			for i := 0; i < cont_d; i++ {
-				resultado += n1[i]
-			}
-			if cont_d > 1 {
-				resultado2 := strings.Split(resultado, "")
-				for i := 0; i < len(resultado2); i++ {
-					if i == 0 {
-						resultado = resultado2[0] + "."
-					} else if i != 0 {
-						resultado += resultado2[i]
-					}
-				}
-				resultado = resultado + "M"
-			} else {
-
-				resultado = resultado + "M"
-			}
-		} else if cont_0 == 9 {
-			for i := 0; i < cont_d; i++ {
-				resultado += n1[i]
-			}
-			if cont_d > 1 {
-				resultado2 := strings.Split(resultado, "")
-				for i := 0; i < len(resultado2); i++ {
-					if i == 0 {
-						resultado = resultado2[0] + "."
-					} else if i != 0 {
-						resultado += resultado2[i]
-					}
-				}
-				resultado = resultado + "G"
-			} else {
-
-				resultado = resultado + "G"
-			}
+			generalResult = result(countd, n1, "Z", count0, 24)
 		}
-
 	}
 
-	fmt.Println(resultado)
+	fmt.Println(generalResult)
+}
+
+func result(countd int, n1 []string, letter string, count0 int, quantity int) (generalResult string) {
+	for i := 0; i < countd; i++ {
+		generalResult += n1[i]
+	}
+	generalResult2 := strings.Split(generalResult, "")
+
+	if count0 > quantity {
+		generalResult = ""
+		for i := 0; i < len(n1)-quantity; i++ {
+			generalResult += n1[i]
+		}
+		generalResult = generalResult + letter
+
+	} else if countd > 1 {
+		for i := 0; i < len(generalResult2); i++ {
+
+			if i == 0 {
+				generalResult = generalResult2[0] + "."
+			} else if i != 0 {
+				generalResult += generalResult2[i]
+			}
+		}
+		generalResult = generalResult + letter
+
+	} else {
+		generalResult = generalResult + letter
+
+	}
+	return
+}
+
+func result2(countd int, n1 []string, letter string) (generalResult string) {
+	for i := 0; i < countd; i++ {
+		generalResult += n1[i]
+	}
+	if countd > 1 {
+		generalResult2 := strings.Split(generalResult, "")
+
+		generalResult = generalResult2[0] + "." + generalResult2[1] + letter
+
+	} else {
+
+		generalResult = generalResult + letter
+
+	}
+	return
 }
