4a66ce13c9ae5e1095a7f10f69e331689db28242
MateoCaicedoW <mcaicedo@wawand.co>
2022-05-26 17:18:28 -0500

    uuid

--- Go/apiGo/apiWeb.go
+++ Go/apiGo/apiWeb.go
@@ -8,7 +8,7 @@ import (
 )
 
 type Player struct {
-	ID        string
+	ID        uuid.UUID
 	FirstName string
 	LastName  string
 	Level     int64
@@ -24,17 +24,17 @@ func main() {
 func Json(w http.ResponseWriter, r *http.Request) {
 
 	player := Players{
-		Player{uuid.New().String(), "Juan", "Hernandez", 12},
-		Player{uuid.New().String(), "Marcos", "Llorente", 90},
-		Player{uuid.New().String(), "Luis", "Avila", 82},
-		Player{uuid.New().String(), "Alberto", "Mercado", 20},
-		Player{uuid.New().String(), "Rosa", "Mercado", 30},
-		Player{uuid.New().String(), "Marta", "Rosario", 30},
-		Player{uuid.New().String(), "Isaias", "Perez", 40},
-		Player{uuid.New().String(), "Samuel", "Benitez", 45},
-		Player{uuid.New().String(), "Gonzalo", "Higuain", 25},
-		Player{uuid.New().String(), "Alberto", "Rosado", 75},
-		Player{uuid.New().String(), "Ismael", "Perez", 59},
+		Player{uuid.New(), "Juan", "Hernandez", 12},
+		Player{uuid.New(), "Marcos", "Llorente", 90},
+		Player{uuid.New(), "Luis", "Avila", 82},
+		Player{uuid.New(), "Alberto", "Mercado", 20},
+		Player{uuid.New(), "Rosa", "Mercado", 30},
+		Player{uuid.New(), "Marta", "Rosario", 30},
+		Player{uuid.New(), "Isaias", "Perez", 40},
+		Player{uuid.New(), "Samuel", "Benitez", 45},
+		Player{uuid.New(), "Gonzalo", "Higuain", 25},
+		Player{uuid.New(), "Alberto", "Rosado", 75},
+		Player{uuid.New(), "Ismael", "Perez", 59},
 	}
 
 	//fmt.Println(player[0].FirstName)
