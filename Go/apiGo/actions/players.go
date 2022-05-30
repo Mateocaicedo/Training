41dc45870c9b0d8eb849cc0ac68b66c4af585c84
Mateocaicedo <caicedomateo9@gmail.com>
2022-05-30 18:32:50 -0500

    apiGo

--- Go/apiGo/actions/players.go
+++ Go/apiGo/actions/players.go
@@ -4,29 +4,83 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 	"github.com/mateo/apiGo/models"
 )
 
-func List(w http.ResponseWriter, r *http.Request) {
+func Show(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	err := json.NewEncoder(w).Encode(models.Template{Status: 200, Data: models.Players(), Message: ""})
+	err := json.NewEncoder(w).Encode(models.Template{Status: 200, Data: models.P, Message: ""})
 	if err != nil {
 		json.NewEncoder(w).Encode(models.Template{Status: 400, Data: models.ListPlayers{}, Message: "Bad Request"})
 	}
 }
 
-func ListID(w http.ResponseWriter, r *http.Request) {
+func ShowID(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	players := models.Players()
+	players := models.P
 	//log.Println(params["id"])
 	for i := 0; i < len(players); i++ {
-		if params["id"] == players[i].ID {
+		if params["id"] == players[i].ID.String() {
 			json.NewEncoder(w).Encode(models.Template{Status: 200, Data: models.ListPlayers{players[i]}, Message: ""})
 			return
 		}
 	}
 	json.NewEncoder(w).Encode(&models.Template{Status: 400, Data: models.ListPlayers{}, Message: "Player was not found"})
+
+}
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for i, item := range models.P {
+		if params["id"] == item.ID.String() {
+			models.P = append(models.P[:i], models.P[i+1:]...)
+			json.NewEncoder(w).Encode(models.Template{Status: 200, Data: models.P, Message: ""})
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&models.Template{Status: 400, Data: models.ListPlayers{}, Message: "ID is invalid"})
+
+}
+
+func Create(w http.ResponseWriter, r *http.Request) {
+	players := models.P
+	var newPlayers models.Player
+	json.NewDecoder(r.Body).Decode(&newPlayers)
+	newPlayers.ID = uuid.New()
+	models.P = append(players, newPlayers)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(models.Template{Status: 200, Data: models.P, Message: ""})
+
+}
+
+func Update(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	var tempUpdate models.Player
+	json.NewDecoder(r.Body).Decode(&tempUpdate)
+
+	for i, item := range models.P {
+		if params["id"] == item.ID.String() {
+			if tempUpdate.FirstName != "" {
+				models.P[i].FirstName = tempUpdate.FirstName
+			}
+			if tempUpdate.LastName != "" {
+				models.P[i].LastName = tempUpdate.LastName
+			}
+
+			if tempUpdate.Level != 0 {
+				models.P[i].Level = tempUpdate.Level
+			}
+
+			json.NewEncoder(w).Encode(models.Template{Status: 200, Data: models.P, Message: ""})
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&models.Template{Status: 400, Data: models.ListPlayers{}, Message: "ID is invalid"})
 
 }
--- Go/apiGo/main.go
+++ Go/apiGo/main.go
@@ -10,8 +10,11 @@ import (
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/players", actions.List).Methods(http.MethodGet)
-	r.HandleFunc("/players/{id}", actions.ListID).Methods(http.MethodGet)
+	r.HandleFunc("/players", actions.Show).Methods(http.MethodGet)
+	r.HandleFunc("/players/{id}", actions.ShowID).Methods(http.MethodGet)
+	r.HandleFunc("/players", actions.Create).Methods(http.MethodPost)
+	r.HandleFunc("/players/{id}", actions.Delete).Methods(http.MethodDelete)
+	r.HandleFunc("/players/{id}", actions.Update).Methods(http.MethodPut)
 
 	server := &http.Server{
 		Addr:    ":3000",
